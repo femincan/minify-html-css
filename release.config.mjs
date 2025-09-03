ad882b9ae010cc44f06d66d4f317f90495584994
Furkan Emin Can <femincanx@gmail.com>
2025-09-03 14:20:40 +0300

    ci: update semantic-release config to patch release when docs change

--- release.config.mjs
+++ release.config.mjs
@@ -4,7 +4,18 @@
 export default {
 	branches: ['main'],
 	plugins: [
-		'@semantic-release/commit-analyzer',
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'angular',
+				releaseRules: [
+					{
+						type: 'docs',
+						release: 'patch',
+					},
+				],
+			},
+		],
 		'@semantic-release/release-notes-generator',
 		'@semantic-release/changelog',
 		'@semantic-release/npm',
